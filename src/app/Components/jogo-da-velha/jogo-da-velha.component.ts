a890583764a7337a6669aca4b8a56bdfb7d180ae
matheusgevartosky <magevartosky@gmail.com>
2022-05-04 17:47:13 -0300

    Lista-Semana-9 --Angular-DevinHouse

--- src/app/Components/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/Components/jogo-da-velha/jogo-da-velha.component.ts
@@ -21,8 +21,17 @@ export class JogoDaVelhaComponent implements OnInit {
   }
 
   public play(line:number, column:number){
-    //console.log(`linha ${line} coluna ${column}`)
+    //this.board[line][column] = this.currentPlayer
+
+
     if(this.board[line][column] == "" && this.winner == ''){
+
+      this.board[line][column] = this.currentPlayer;
+
+      if(this.checkWinner(this.currentPlayer)){
+        this.winner = this.currentPlayer;
+      }
+
       if(this.currentPlayer == 'O'){
         this.currentPlayer = 'X'
       }else{
@@ -30,10 +39,35 @@ export class JogoDaVelhaComponent implements OnInit {
       }
     }
 
+
   }
 
-  public checkWinner() {
-    return;
+  public checkWinner(player: string): boolean {
+    for(let i = 0; i < this.board.length; i++){
+      if(this.board[i][0] == player &&  this.board [i][1] == player &&this.board [i][2] == player){
+        return true
+      }
+    }for(let i = 0; i < this.board.length; i++){
+      if(this.board[0][i] == player &&  this.board [1][i] == player &&this.board [2][i] == player){
+        return true
+      }
+    }
+
+    if(this.board[0][0] == player && this.board[1][1] == player && this.board[2][2]){
+      return true
+    }if(this.board [0][2] ==player && this.board[1][1] == player && this.board[2][0] == player){
+      return true
+    }
+    return false;
   }
 
+  public reset(){
+  this.currentPlayer = "O";
+  this.winner = '';
+  this.board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
+}
 }
